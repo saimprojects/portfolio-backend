24f1e2f9b239964e4fcb924f47e25462e1bd4953
saimprojects <saimpkf@gmail.com>
2025-07-15 22:39:40 -0700

    image upload fixed

--- core/serializers.py
+++ core/serializers.py
@@ -2,12 +2,14 @@ from rest_framework import serializers
 from .models import Project, Blog, Skill, Contact, Service
 
 class ProjectSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()  # ✅ Yeh zaroori hai!
+
     class Meta:
         model = Project
         fields = '__all__'
-    def get_image(self, obj):
-        return obj.image.url     
 
+    def get_image(self, obj):
+        return obj.image.url
 
 class BlogSerializer(serializers.ModelSerializer):
     class Meta:
